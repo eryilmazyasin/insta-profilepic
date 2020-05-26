ae2d1d9e1cca4b4df4f99f38d87263910b445829
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2020-05-26 14:29:20 +0300

    user component düzenlendi

--- src/User.js
+++ src/User.js
@@ -2,11 +2,15 @@ import React from "react";
 
 
 export default function TotalData(props) {
+  if(props.instagram.profile_pic_url_hd){
+    return <img src={props.instagram.profile_pic_url_hd} alt="insta_photo"/>
+  }
+  else {
+    return <div>Henüz bir kullanıcı aratılmadı!</div>
+  }
   return ( 
-
-    
     <div>
-        <img src={props.instagram.profile_pic_url_hd} alt="insta_photo"/>
+        
     </div>
 
     
